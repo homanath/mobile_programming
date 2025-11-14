d468d566831ccd76dc50076e4f50f613a5c1c3de
homanath <suzanadhikari101@gmail.com>
2025-11-14 07:19:18 +0545

    added water droplet sound and color change animation in alert dialog box

--- AlertDialogExample/app/src/main/java/com/example/alertdialogexample/MainActivity.java
+++ AlertDialogExample/app/src/main/java/com/example/alertdialogexample/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.alertdialogexample;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.widget.Button;
+import android.widget.Toast;
+import android.graphics.Color;
+import java.util.Random;
+import android.media.MediaPlayer;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +20,35 @@ public class MainActivity extends AppCompatActivity {
         Button btnShow = findViewById(R.id.btnShowDialog);
 
         btnShow.setOnClickListener(v -> {
+
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("Alert Message");
             builder.setMessage("This is a simple Alert Dialog example.");
 
             builder.setPositiveButton("OK", (dialog, which) -> {
-                // Action when OK pressed
+
+                Toast.makeText(MainActivity.this, "🎉 You pressed OK!", Toast.LENGTH_SHORT).show();
+
+                // Play water droplet sound
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.water_drop);
+                mp.start();
+
+                // Change button label
+                btnShow.setText("You clicked OK 😄");
+
+                // Change button background color randomly
+                int[] colors = { Color.RED, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };
+                int randomColor = colors[new Random().nextInt(colors.length)];
+                btnShow.setBackgroundColor(randomColor);
+
+                // Return to first screen after 5 seconds
+                new Handler().postDelayed(() -> {
+                    recreate();
+                }, 5000);
+
             });
 
-            builder.setNegativeButton("Cancel", (dialog, which) -> {
-                dialog.dismiss();
-            });
+            builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
 
             builder.create().show();
         });
